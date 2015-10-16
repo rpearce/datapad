b64bc7d08b2cd25bdc2565ca18f8c8eeb5090e6f
Robert Pearce <me@robertwpearce.com>
2015-10-15 22:19:50 -0400

    Refactored scraper a bit

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -1,4 +1,12 @@
 class Scraper
+  SELECTORS = {
+    category_links: 'a.CategoryTreeLabel',
+    article_links: '[class="mw-content-ltr"] a',
+    name: '#WikiaPageHeader h1',
+    image: '.infoboximage img',
+    summary: '#mw-content-text > p'
+  }.freeze
+
   def initialize
     @agent = Mechanize.new { |a| a.user_agent_alias = 'Mac Safari' }
   end
@@ -9,41 +17,41 @@ class Scraper
   end
 
   def drill_down(page)
-    category_links = get_category_links(page)
-    article_links = get_article_links(page) if category_links.empty?
+    category_links = get_links(page: page, selector: SELECTORS[:category_links])
+    article_links = get_links(page: page, selector: SELECTORS[:article_links]) if category_links.empty?
 
     if category_links.empty? && !article_links.empty?
       article_links.each { |link| drill_down(@agent.get(link)) }
     elsif category_links.empty? && article_links.empty?
-      c = Character.create(
-        name: get_name(page),
-        image: get_image(page),
-        summary: get_summary(page),
-        external_uri: page.uri.to_s
-      )
-      puts c.name if c.persisted?
+      create_character(page)
     else
-      category_links.each { |link| drill_down(link.click) }
+      category_links.each { |link| drill_down(@agent.get(link)) }
     end
   end
 
-  def get_category_links(page)
-    page.links.select { |l| /CategoryTreeLabel/.match(l.dom_class) }
-  end
-
-  def get_article_links(page)
-    page.search('[class="mw-content-ltr"] a').map { |a| a["href"] }
+  def get_links(page:, selector:)
+    page.search(selector).map { |a| a['href'] }
   end
 
   def get_name(page)
-    page.search('#WikiaPageHeader h1').first.text
+    page.search(SELECTORS[:name]).first.text
   end
 
   def get_image(page)
-    page.search('.infoboximage img').first.try(:[], 'src')
+    page.search(SELECTORS[:image]).first.try(:[], 'src')
   end
 
   def get_summary(page)
-    page.search('#mw-content-text > p')[0...2].map { |p| p.text.chomp }.join("\n")
+    page.search(SELECTORS[:summary])[0...2].map { |p| p.text.chomp }.join("\n")
+  end
+
+  def create_character(page)
+    c = Character.create(
+      name: get_name(page),
+      image: get_image(page),
+      summary: get_summary(page),
+      external_uri: page.uri.to_s
+    )
+    puts c.name if c.persisted?
   end
 end
