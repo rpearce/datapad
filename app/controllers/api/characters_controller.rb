5a5e82ee29b6cbff81ca6cc21435bccc363de046
Robert Pearce <me@robertwpearce.com>
2015-10-19 13:31:09 -0400

    foreman update and fixed type issue

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -3,8 +3,8 @@ class Api::CharactersController < ApplicationController
     respond_to do |format|
       format.json {
         @characters = Character.includes([:affiliations, :character_affiliations])
-        @characters = @characters.where('name ILIKE ?', "%#{params[:q]}%") if params[:q]
-        @characters = @characters.where(affiliations: { name: params[:type] }) if params[:type]
+        @characters = @characters.where('characters.name ILIKE ?', "%#{params[:q]}%") if params[:q]
+        @characters = @characters.where('affiliations.name = ?', params[:type]).references(:affiliations) if params[:type]
       }
     end
   end
